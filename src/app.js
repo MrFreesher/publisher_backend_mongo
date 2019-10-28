f9eb256bb8da2532f15a5597a25230282e3bcc5c
MrFreesher <artur260@op.pl>
2019-10-28 22:03:25 +0100

    Add pagination

--- src/app.js
+++ src/app.js
@@ -2,13 +2,16 @@ const express = require('express');
 const morgan = require('morgan');
 require('dotenv').config();
 const uploadRouter = require('./routes/uploads');
-
+const magazineRouter = require('./routes/magazine');
 const app = express();
 
 app.use(morgan('dev'));
 app.use('/upload', uploadRouter);
+app.use('/magazines', magazineRouter);
 app.get('/test', (req, res) => {
   res.send({ message: "It's working" });
 });
+
+
 
 app.listen(process.env.PORT || 3000, () => console.log('Server is running...'));
--- src/routes/magazine.js
+++ src/routes/magazine.js
@@ -1,3 +1,23 @@
 const router = require('express').Router();
 const mongoose = require('mongoose');
 const magazine = require('../models/magazine');
+
+router.get('/', async (req, res) => {
+  let query = magazine.find();
+  const { page, limit, title } = req.query;
+  const startIndex = (page - 1) * limit;
+  const endIndex = page * limit;
+
+  try {
+    const magazines = await query
+      .skip(startIndex)
+      .limit(endIndex)
+      .exec();
+    res.send(magazines);
+  } catch (err) {
+    console.error(err);
+    res.send({ message: 'Error' });
+  }
+});
+
+module.exports = router;
