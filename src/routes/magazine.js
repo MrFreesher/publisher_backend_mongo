0e98cbb7f0c08e745bc6535e89dafc5a5a89343a
MrFreesher <artur260@op.pl>
2019-12-01 22:07:57 +0100

    Add filter by title 1

--- src/routes/magazine.js
+++ src/routes/magazine.js
@@ -3,18 +3,24 @@ const mongoose = require('mongoose');
 const magazine = require('../models/magazine');
 
 router.get('/', async (req, res) => {
-  let query = magazine.find();
+  const query = {};
   const { page, limit, title } = req.body;
   console.log(req.body);
   const startIndex = (page - 1) * limit;
   const endIndex = page * limit;
-
+  if (title !== 'undefined' || title !== '') {
+    const reg = new RegExp(`^${title}`);
+    query.Tytul1 = reg;
+    console.log(query);
+  }
   try {
-    const magazines = await query
+    const magazines = await magazine
+      .find(query)
       .skip(startIndex)
       .limit(endIndex)
       .exec();
-    res.send(magazines);
+
+    await res.send({ lista: magazines });
   } catch (err) {
     console.log(err);
     res.send({ message: 'Error' });
